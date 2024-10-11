f2a5128dda15e52152846d8165049d5fec9001a5
MrMurder4all <64412699+MrMurder4all@users.noreply.github.com>
2024-10-11 18:17:46 +0300

    changed recieve_message
    Изменены функция recieve_message и main, чтобы разобрать входной поток данных в структуру сообщения.

--- task2.c
+++ task2.c
@@ -3,7 +3,7 @@
 #include <stdbool.h>
 #include <string.h>
 
-#define MAX_DATA_LENGTH 255 //максимум 255 байт
+#define MAX_DATA_LENGTH 255 // максимум 255 байт
 
 // Структура для хранения сообщения
 typedef struct {
@@ -28,11 +28,11 @@ void init_protocol_message(ProtocolMessage *msg) {
 // Вычисление CRC
 uint8_t calculate_crc(const ProtocolMessage *msg) {
     uint8_t crc = 0;
-    
+
     for (int i = 0; i < sizeof(msg->preamble); i++) {
         crc ^= msg->preamble[i];
     }
-    
+
     crc ^= msg->length;
     crc ^= msg->src_addr;
     crc ^= msg->dest_addr;
@@ -49,36 +49,73 @@ bool verify_crc(const ProtocolMessage *msg) {
     return calculate_crc(msg) == msg->crc;
 }
 
-// получение сообщения из входного потока (симуляция)
-bool receive_message(ProtocolMessage *msg, uint32_t timeout_ms) {
-    // Симуляция получения данных
-    
-    // (в реальной реализации здесь будет код для чтения данных)
+// Получение сообщения из входного потока (по одному байту)
+bool receive_message(ProtocolMessage *msg) {
     init_protocol_message(msg);
+
+    // Чтение преамбулы
+    for (int i = 0; i < sizeof(msg->preamble); i++) {
+        if (fscanf(stdin, "%hhx", &msg->preamble[i]) != 1) { // Считываем в шестнадцатеричном формате
+            return false; 
+        }
+    }
+
+    // Чтение длины сообщения
+    if (fscanf(stdin, "%hhu", &msg->length) != 1) { // Считываем длину
+        return false; 
+    }
+
+    // Чтение адреса источника и адреса получателя
+    if (fscanf(stdin, "%hhx %hhx", &msg->src_addr, &msg->dest_addr) != 2) {
+        return false; 
+    }
+
+    // Чтение данных
+    if (msg->length > MAX_DATA_LENGTH) {
+        return false; 
+    }
     
-    memcpy(msg->preamble, (uint8_t[]){0x01, 0x02, 0x03, 0x04}, sizeof(msg->preamble));
-    
-    msg->length = 3; // Пример длины данных
-    msg->src_addr = 0x10; // Пример адреса источника
-    msg->dest_addr = 0x20; // Пример адреса приёмника
-    
-    memcpy(msg->data, (uint8_t[]){0xAA, 0xBB, 0xCC}, msg->length);
-    
+    for (int i = 0; i < msg->length; i++) {
+        if (fscanf(stdin, "%hhx", &msg->data[i]) != 1) { // Считываем данные
+            return false; 
+        }
+    }
+
     // Вычисление CRC и его добавление в сообщение
     msg->crc = calculate_crc(msg);
-    
+
     return true;
 }
 
 int main() {
     ProtocolMessage message;
 
-    if (receive_message(&message, 1000)) {
+    // Пояснение о формате ввода
+    printf("Please enter the message in the following format:\nPreamble (4 bytes): 0x01 0x02 0x03 0x04\nLength of message (1 byte): N (number of data bytes)\n");
+    printf("Source Address (1 byte): 0x10\nDestination Address (1 byte): 0x20\nData (N bytes): 0xAA 0xBB 0xCC\n");
+    printf("Example input: 01 02 03 04 03 10 20 AA BB CC\n");
+
+    if (receive_message(&message)) {
         if (verify_crc(&message)) {
             printf("Message successfully received and verified.\n");
         } else {
             printf("CRC check error.\n");
         }
+        
+        // Вывод информации о сообщении(на всякий случай)
+        printf("Preamble: ");
+        for (int i = 0; i < sizeof(message.preamble); i++) {
+            printf("%02X ", message.preamble[i]);
+        }
+        printf("\nLength: %d\n", message.length);
+        printf("Source Address: %02X\n", message.src_addr);
+        printf("Destination Address: %02X\n", message.dest_addr);
+        printf("Data: ");
+        for (int i = 0; i < message.length; i++) {
+            printf("%02X ", message.data[i]);
+        }
+        printf("\nCRC: %02X\n", message.crc);
+        
     } else {
         printf("Message received error.\n");
     }
